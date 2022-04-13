1536679b00de6ee6f654923ce9cbf3cb05b66842
Alejandro9509 <alejandrobringas2021@gmail.com>
2022-04-13 00:41:35 -0700

    Adding latest changes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import React, { createRef, useState } from 'react';
 import { marked } from 'marked';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Modal} from 'react-bootstrap'
+import {Button, Modal, Table} from 'react-bootstrap'
+
+
+const defaultValue = "# Presentacion basica de Markdown <br>\n Este proyecto fue gracias a la herramienta de [Marked] \n"
+      defaultValue += "y tambien en conjunto de **bibliotecas** integradas en este proyecto como:\n"
+      defaultValue += "- React-Bootstrap\n- React\n\n"
+      defaultValue += "[Marked]: https://github.com/markedjs/marked/"
+
 
 const showExample = [
-  ['Ejemplo 1 <br>======', marked.parse('Ejemplo 1 \n======')],
-  ['Ejemplo 2 <br>------', marked.parse('Ejemplo 2 \n------')]
+  'Texto<br>\n======',
+  'Texto<br>\n---------',
+  '**Texto** ',
+  '- Texto<br>\n- Texto',
+  '[Link] <br><br>\n\n [Link]: https://github.com/Alejandro9509/Alejandro9509'
 ]
-
-
 function ModalTutorial(){
   const [show, setShow] = useState(false)
   return (
@@ -18,17 +26,27 @@ function ModalTutorial(){
       <Button onClick={() => setShow(true)}>Mostrar tutorial</Button>
       <Modal show={show} onHide={() => setShow(false)}>
           <Modal.Header>
-            <Modal.Title>Cobrar</Modal.Title>
+            <Modal.Title>Tutorial</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-           {
-             showExample.map((i) =>
-               <div className='row'>
-                <div className='col-sm' dangerouslySetInnerHTML={{ __html: i[0] }}></div>
-                <div className='col-sm' dangerouslySetInnerHTML={{ __html: i[1] }}></div>
-               </div>
-              )
-           }
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Ejemplo</th>
+                  <th>Resultado</th>
+                </tr>
+              </thead>
+              <tbody>
+               {
+                 showExample.map((i) =>
+                   <tr key={i}>
+                      <td className='col-sm' dangerouslySetInnerHTML={{ __html: i }}></td>
+                      <td className='col-sm' dangerouslySetInnerHTML={{ __html: marked.parse(i) }}></td>
+                   </tr>
+                  )
+               }
+             </tbody>
+           </Table>
           </Modal.Body>
       </Modal>
     </React.Fragment>
@@ -60,7 +78,8 @@ class App extends React.Component {
         <h1>Markdown basico en ReactJS</h1>
         <ModalTutorial />
         <div className="md-form row" style={{marginTop: 20}}>
-          <textarea defaultValue="# h1" ref={this.myRef} id="editor" className='md-textarea form-control col-sm' cols='50' onChange={this.txt}></textarea>
+          <textarea defaultValue={defaultValue} ref={this.myRef} id="editor" className='md-textarea form-control col-sm' rows="10" onChange={this.txt}></textarea>
+
           <div className='col-sm' id="preview" dangerouslySetInnerHTML={{ __html: marked.parse(this.state.html) }}></div>
         </div>
 
