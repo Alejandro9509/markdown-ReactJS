46c3857d0146dabe28e018f2671bd10527c4d433
Alejandro9509 <alejandrobringas2021@gmail.com>
2022-04-12 23:25:47 -0700

    Adding tutorial button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 
 import './App.css';
-import React, { createRef } from 'react';
+import React, { createRef, useState } from 'react';
 import { marked } from 'marked';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Modal} from 'react-bootstrap'
 
-//React
+const showExample = [
+  ['Ejemplo 1 <br>======', marked.parse('Ejemplo 1 \n======')],
+  ['Ejemplo 2 <br>------', marked.parse('Ejemplo 2 \n------')]
+]
+
+
+function ModalTutorial(){
+  const [show, setShow] = useState(false)
+  return (
+    <React.Fragment>
+      <Button onClick={() => setShow(true)}>Mostrar tutorial</Button>
+      <Modal show={show} onHide={() => setShow(false)}>
+          <Modal.Header>
+            <Modal.Title>Cobrar</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+           {
+             showExample.map((i) =>
+               <div className='row'>
+                <div className='col-sm' dangerouslySetInnerHTML={{ __html: i[0] }}></div>
+                <div className='col-sm' dangerouslySetInnerHTML={{ __html: i[1] }}></div>
+               </div>
+              )
+           }
+          </Modal.Body>
+      </Modal>
+    </React.Fragment>
+  )
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -14,7 +44,6 @@ class App extends React.Component {
     this.txt = this.getTextAreaValue.bind(this);
     this.myRef = createRef()
   }
-
   getTextAreaValue(event) {
     this.setState({
       html: event.target.value,
@@ -29,12 +58,12 @@ class App extends React.Component {
     return (
       <div className="container-md p-2">
         <h1>Markdown basico en ReactJS</h1>
-        <div className="md-form">
-          <textarea ref={this.myRef} id="editor" className='md-textarea form-control' cols='50' onChange={this.txt}>
-            # h1
-          </textarea>
+        <ModalTutorial />
+        <div className="md-form row" style={{marginTop: 20}}>
+          <textarea defaultValue="# h1" ref={this.myRef} id="editor" className='md-textarea form-control col-sm' cols='50' onChange={this.txt}></textarea>
+          <div className='col-sm' id="preview" dangerouslySetInnerHTML={{ __html: marked.parse(this.state.html) }}></div>
         </div>
-        <div id="preview" dangerouslySetInnerHTML={{ __html: marked.parse(this.state.html) }}></div>
+
       </div>
     )
   }
